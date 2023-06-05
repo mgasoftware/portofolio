2843d6589594cd1643cbe36a7a4b43b2ea2fd60c
mgasoftware <macir.gassama12@gmail.com>
2023-06-05 16:46:06 +0200

    Modified import link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,7 +64,7 @@ export default function Contact() {
                     <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
                         <div className='p-4'>
                             <form
-                                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
+                                action='https://getform.io/f/0911f002-4389-44fd-a308-7b36ec323a18'
                                 method='POST'
                                 encType='multipart/form-data'
                             >
